f49e7a3131c2ecbd3bc820ad57d1a4e1b37efb58
vipinms77 <vipinms77@gmail.com>
2019-12-12 11:49:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wsb4hr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wsb4hr)
---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  numberString;
+  array: any[] = [];
+  search:any;
+  result: boolean;
+  onClick(){
+    if(this.numberString.trim()){
+      this.array = this.numberString.split(',');
+      if(this.array.length > 0 && this.search){
+        this.result = this.binarySearch(this.array,this.search);
+      }
+    } 
+  }
+  binarySearch(arr:any[], search){
+      let left = 0;
+      let right = arr.length - 1;
+      arr.sort();
+      while (left <= right) {
+          const mid = left + Math.floor((right - left) / 2);
+          if (arr[mid] == search) {
+              return true;
+          }
+          if (arr[mid] < search) {
+              left = mid + 1;
+          } else {
+              right = mid - 1;
+          }
+      }
+      return false;
+  }
+}
